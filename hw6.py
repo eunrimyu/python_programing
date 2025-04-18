ec3e3b5cfc4d6f7776be55401974fa68e9fefb35
eunrimyu <eunrim2004@naver.com>
2025-04-18 18:00:19 +0900

    6차 과제

---
+++ hw6.py
@@ -0,0 +1,17 @@
+
+#개념확인 문제
+for j in range(1, 10):
+    for i in range (2, 6):
+        k = i*j
+        print(f"{i}*{j}={k}", end ="\t")
+    print() #줄바꿈
+
+print("\n")
+
+for j in range(1, 10):  
+    for i in range (6, 10):
+        k = i*j
+        print(f"{i}*{j}={k}", end="\t")
+    print() #줄바꿈
+
+
